651fdd8e5a9273b5462943ae4c9e27fec99ae386
Phạm Hồ Mạnh Tú <88744368+StewAway@users.noreply.github.com>
2024-06-04 00:03:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/api/product", require('./routes/product.js'));
 
 
 //connect database
-mongoose.connect("mongodb+srv://manhtuphamho:zbdeEEnqfy1TGJl6@simpledb.1h1mgeq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Simpledb")
+mongoose.connect("mongodb+srv://<username>.<password>mongodb.net/Node-API?retryWrites=true&w=majority&appName=Simpledb")
 .then(() => {
     console.log("Connected to database!");
     app.listen(3000, () => {   console.log('Server is running on http://localhost:3000');});
